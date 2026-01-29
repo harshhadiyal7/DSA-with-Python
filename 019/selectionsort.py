fc533f3d4a8e88da0418a12c5acbe56ae4802b4b
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-01-29 16:31:32 +0530

    selectionsort algo

---
+++ 019/selectionsort.py
@@ -0,0 +1,17 @@
+# Selection Sort Implementation.
+nums = [1,7,8,4,5,6,9,2]
+def selection_sort(nums):
+    n=len(nums)
+    for i in range(0, n):
+        min_ind=i
+        for j in range(i+1, n):
+            if nums[j]<nums[min_ind]:
+                min_ind=j
+        nums[i], nums[min_ind]=nums[min_ind], nums[i]
+selection_sort(nums)
+print(nums)
+
+'''
+TC: O(n^2)
+SC: O(1)
+'''
