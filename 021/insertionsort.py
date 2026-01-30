b8bb5808ba921613979e3b8ade22af3bedcfbf39
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-01-30 11:33:36 +0530

    insertion sor

---
+++ 021/insertionsort.py
@@ -0,0 +1,15 @@
+# Insertionsort implementation in Python.
+
+nums = [3,5,6,4,8,9,10,7,1]
+
+n = len(nums)
+for i in range (1, n):
+    key = nums[i]
+    j=i-1
+    
+    while j>=0 and nums[j]>key:
+        nums[j+1] = nums[j]
+        j -= 1
+        
+    nums[j+1] = key
+print("Sorted array is:", nums)
