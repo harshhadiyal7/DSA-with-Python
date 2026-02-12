1a215f0aac2a891beccd53dc3fd0e8238905f072
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-12 14:10:07 +0530

    Two sum.

--- 035/main.py
+++ 035/main.py
@@ -12,3 +12,21 @@ for i in range (0, n-1):
     for j in range(i+1,n):
         if nums[i]+nums[j] == target:
             print(i,j)
+'''
+TC: O(n^2)
+SC: O(1)
+'''          
+            
+#Optimal solution
+nums = [5,9,1,2,4,15,6,3]
+n=len(nums)
+hash_map={}
+for i in range(0,n):
+    remaining = target-nums[i]
+    if remaining in hash_map:
+        print(hash_map[remaining],i)
+    hash_map[nums[i]]=i
+'''
+TC: O(n)
+SC: O(n)
+'''
