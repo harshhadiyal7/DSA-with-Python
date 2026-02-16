ebe2e936ef3ef3f201623e2e9dfa837a701a2108
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-16 12:44:18 +0530

    Best Time to Buy and Sell Stock(leetcode 121).

--- 037/main.py
+++ 037/main.py
@@ -1,1 +1,22 @@
 #Best Time to Buy and Sell Stock(leetcode 121).
+'''
+prise = [7,2,1,5,6,4,8]
+we find lowest price to buy, and highest price to sell and return meximum profit.
+'''
+#Brute force solution
+prise = [7,2,1,5,6,4,8]
+n=len(prise)
+max_profit=0
+
+for i in range (0, n):
+    for j in range(i+1, n):
+        if prise[j] > prise[i]:
+            p= prise[j]-prise[i]
+            if p > max_profit:
+                max_profit = p
+
+print(f"The maximum profit is: {max_profit}")
+'''
+TC : O(n^2)
+SC : O(1)
+'''
